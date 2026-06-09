32a9ff092ad0520d95a57520fa3d981d9c654396
xiaobu <864181495@qq.com>
2026-06-09 12:21:39 +0800

    feat: 考试管理添加查询功能

--- apps/exam/views.py
+++ apps/exam/views.py
@@ -3,6 +3,7 @@
 
 from rest_framework.permissions import IsAuthenticated
 
+from apps.core.pagination import StandardResultsSetPagination
 from apps.core.viewsets import BaseViewSet
 from apps.exam.models import ExamPlan
 from apps.exam.serializers import ExamPlanSerializer
@@ -12,3 +13,21 @@ class ExamPlanViewSet(BaseViewSet):
     queryset = ExamPlan.objects.all()
     serializer_class = ExamPlanSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        qs = ExamPlan.objects.select_related('semester')
+
+        exam_type = self.request.query_params.get('exam_type', '').strip()
+        if exam_type:
+            qs = qs.filter(exam_type=exam_type)
+
+        grade = self.request.query_params.get('grade', '').strip()
+        if grade:
+            qs = qs.filter(grade=grade)
+
+        semester = self.request.query_params.get('semester', '').strip()
+        if semester:
+            qs = qs.filter(semester_id=semester)
+
+        return qs
